1cc79668d4b4b1a9bb6d504427037ed30ea4628a
hoollyzhang <hoollyzhang@tencent.com>
2020-04-05 13:43:21 +0800

    【新增】初始化项目

--- cloudfunctions/news/index.js
+++ cloudfunctions/news/index.js
@@ -1,16 +1,28 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
+const axios = require('axios')
 cloud.init()
 
+async function getNews(category,hot_type,count) {
+  console.log('start getNews')
+  let data = {}
+  try {
+    const url = `https://gank.io/api/v2/hot/${hot_type}/category/${category}/count/${count}`
+    console.log(url)
+    var res = await axios.get(url)
+    data = res.data.data
+  } catch (err) {
+    console.log(err)
+  }
+  return data
+}
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  console.log(event)
+  const data =  await getNews(event.category,event.hot_type,event.count)
+  console.log(data)
   return {
     event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    data:data
   }
 }
